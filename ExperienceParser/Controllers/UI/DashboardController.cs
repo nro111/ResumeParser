0dac92c6946976526c7c28ad891c77d7e0b64886
Nick Ormsby <normsby@psahealthcare.com>
2017-05-17 21:17:33 -0400

    Updated UI and setup DashboardController

--- ExperienceParser/Controllers/UI/DashboardController.cs
+++ ExperienceParser/Controllers/UI/DashboardController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ExperienceParser.Controllers.UI
 {
     public class DashboardController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Recruiter()
         {
-            ViewBag.Title = "Home Page";
+            return View();
+        }
 
+        public ActionResult Applicant()
+        {
             return View();
         }
     }
